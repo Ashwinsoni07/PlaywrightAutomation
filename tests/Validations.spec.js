db2d5630d0a8f1e7be3e88e72415b912e3cc6331
Ashwin <soni.ash2002@gmail.com>
2026-08-01 20:34:42 +0530

    Excel Handling

--- tests/Validations.spec.js
+++ tests/Validations.spec.js
@@ -6,13 +6,29 @@ test('Popup validations', async ({ page }) => {
     // await page.goBack();
     // await page.goForward();
 
-    // await page.locator('#alertbtn').click();
+    await page.locator('#alertbtn').click();
     page.on('dialog', dialog => dialog.accept());
     await page.locator('#confirmbtn').click();
     // page.on('dialog', dialog => dialog.dismiss());
 
     await page.locator('#mousehover').hover();
 
+});
 
+test('Screenshot & Visual comparisions', async ({ page }) => {
+
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator('#displayed-text')).toBeVisible();
+    await page.locator('#displayed-text').screenshot({ path: 'partialScreenshot.png' });
+    await page.locator('#hide-textbox').click();
+    await page.screenshot({ path: 'screenshot.png' });
+    await expect(page.locator("displayed-text")).toBeHidden();
+
+
+});
+test("test Visuals", async ({ page }) => {
+
+    await page.goto("https://www.flightaware.com/");
+    expect(await page.screenshot()).toMatchSnapshot("landingPage.png");
 
 });
---
+++ tests/excel/Exceldemo.js
@@ -0,0 +1,46 @@
+const ExcelJS = require('exceljs');
+
+
+const workbook = new ExcelJS.Workbook();
+/**This below method is a asynchronous hence JS dosn't wait for the file details to be fetched
+ * and continues the execution without the file. To handle such cases it is handled using a then keyword with a 
+ * function defining thelatertasks.
+ */
+// workbook.xlsx.readFile("D:/Development/TestProject/dummyTestFile.xlsx").then(function(){
+//     const worksheet = workbook.getWorksheet('Sheet1');
+//     worksheet.eachRow((row, rowNumber) => {
+//         row.eachCell((cell, colNumber) => {/**Iterating through each row and cell */
+//             console.log(cell.value);
+
+//         })
+//     })
+
+// });
+async function excelTest() {
+
+    let output = { row: -1, column: -1 };
+
+    await workbook.xlsx.readFile("D:/Development/TestProject/dummyTestFile.xlsx");
+    const worksheet = workbook.getWorksheet('Sheet1');
+    worksheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, colNumber) => {/**Iterating through each row and cell */
+            if (cell.value === "Apple") {
+
+                output.row = rowNumber;
+                output.column = colNumber;
+
+            }
+            console.log(cell.value);
+
+        })
+    });
+
+    const cell = worksheet.getCell(output.row, output.column);
+    cell.value = "iphone";
+    await workbook.xlsx.writeFile("D:/Development/TestProject/dummyTestFile.xlsx");
+
+}
+
+excelTest();//calling the defined function
+
+
